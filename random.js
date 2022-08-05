e716ad4fdcd76a0930618a69d9a5c38255d890c9
Anna <momannuta@gmail.com>
2022-08-05 17:49:27 +0300

    edit

--- random.js
+++ random.js
@@ -1,23 +1,35 @@
-function generate(){
-    let arr = [];
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
-    arr.push(Math.round(Math.random()*10));
 
+function generate(){
+    return Math.round(Math.random()*20 - 10);
+}
+
+function generateArray() {
+    let arr = [];
+    for (let i = 0; i < 10; i++){
+        let number = generate();
+        arr.push(number);
+    }
     document.querySelector('.arr').innerHTML ="Сгенерировали: " + arr;
 
-    let min = Math.min(...arr);
-    document.querySelector('.min').innerHTML = "Минимальное: " + min.toString();
+    //нахождение мин и макс через цикл
+/*let min = arr[0];
+let max = arr[0];
+
+for (let i = 0; i < arr.length; i++){
+    let item = arr[i];
+
+    if (item < min) {
+        min = item;
+    } else if (item > max){
+        max = item;
+    }
+}return[min, max];*/
+
+let min = Math.min(...arr);
+document.querySelector('.min').innerHTML = "Минимальное: " + min.toString();
     
-    let max = Math.max(...arr);
-    document.querySelector('.max').innerHTML = "Максимальное: " + max.toString();
+let max = Math.max(...arr);
+document.querySelector('.max').innerHTML = "Максимальное: " + max.toString();
 
     function average(arr) {
         let sum = 0;
@@ -44,5 +56,6 @@ function generate(){
     document.querySelector('.average').innerHTML = "Среднее арифметическое: " + average(arr);
     document.querySelector('.sum').innerHTML = "Сумма чисел: " + sum(arr);
     document.querySelector('.multiply').innerHTML = "Произведение чисел: " + multiply(arr);
-
 }
+
+
